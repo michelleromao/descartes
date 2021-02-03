a018eb5656b23445744b790eba509bd1e35768aa
Michelle Romão <michelle.nunes10@gmail.com>
2021-02-03 01:30:09 -0300

    create tab bar

--- app-descartes/src/App.js
+++ app-descartes/src/App.js
@@ -1,21 +1,21 @@
 import 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 
 import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
 import { View } from 'react-native';
 import Routes from './routes';
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <SafeAreaProvider>
       <View style={{ flex: 1, backgroundColor: '#F1F1F1' }}>
         <Routes />
       </View>
 
       <StatusBar style="auto" />
-    </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 export default App;
