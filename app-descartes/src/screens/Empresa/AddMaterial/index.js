38e13c46980ac0282178c4cdcc77a317747012c0
Jamilly <jamillyp@alu.ufc.br>
2021-03-07 21:15:29 -0300

    feat: Adicionando componente AddMaterial

--- app-descartes/src/screens/Empresa/AddMaterial/index.js
+++ app-descartes/src/screens/Empresa/AddMaterial/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Form } from '@unform/mobile';
+import { Button, View } from 'react-native';
+
+import Button from '../../../components/Button';
+import Imput from '../../../components/Input';
 
 const AddMaterial = () => {
-  return 
-  <View>
-  </View>;
+  return (
+    <View>
+      <Form>
+      <Imput name="Tipo do Material" />
+      <Imput name="Quantidade/Tamanho" />
+      <Imput name="Disponibilidade" />
+
+      <Button 
+      title="+"
+      size=""
+      color=""
+      />
+
+      <Button 
+      title="CANCELAR"
+      size=""
+      color="" 
+      />
+      <Button 
+      title="CADASTRAR"
+      size=""
+      color="" 
+      />
+
+      </Form>
+    </View>
+    
+  );
+ 
 };
 
 export default AddMaterial;
