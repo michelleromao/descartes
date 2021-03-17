be5404e45218ebb909f6a6e0058c5b51f15fd3fe
Michelle Romão <michelle.nunes10@gmail.com>
2021-03-17 00:59:34 -0300

    add landing page

---
+++ web-descartes/src/components/AppSection/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { HashLink as Link } from 'react-router-hash-link';
+
+import App from "../../assets/banner/banner-app.png";
+
+
+
+import { Container } from './styles';
+
+function AppSection() {
+  return (
+    <Container id="app"> 
+      <img src={App} alt="Aquira o aplicativo Descartes" />
+    </Container>
+  );
+}
+
+export default AppSection;
